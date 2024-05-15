e3ce6e696e0a75c1fe834a0265e15424345a6dd6
mhrah <mhrahatulislam@gmail.com>
2024-05-15 12:22:24 +0600

    rahat

--- app/src/main/java/com/example/projectclicnt/LoginActivity.kt
+++ app/src/main/java/com/example/projectclicnt/LoginActivity.kt
@@ -15,6 +15,10 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
 
+        setContentView(binding.root)
+
+        setupListeners()
+
     }
 
     private fun setupListeners() {
